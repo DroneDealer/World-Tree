28babbf46e8a586f790f24566b91833b9fec13c5
DroneDealer <1rastogiani@gmail.com>
2025-06-28 17:57:13 -0400

    added probabilities for fruits to spawn (more points for certain kind of fruit = lower probability)

--- Assets/FruitSpawnScript.cs
+++ Assets/FruitSpawnScript.cs
@@ -2,19 +2,17 @@ using UnityEngine;
 
 public class FruitSpawnScript : MonoBehaviour
 {
-    public GameObject[] fruits;
+    public FruitWeight[] fruits;
     public float spawnRate = 2;
     private float timer = 0;
     public float widthOffset = 14;
 
-    void Start()
-    {
-        spawnFruit();
-    }
-
     // Update is called once per frame
     void Update()
     {
+        if (fruits == null || fruits.Length == 0)
+            return;
+
         if (timer < spawnRate)
         {
             timer += Time.deltaTime;
@@ -28,10 +26,27 @@ public class FruitSpawnScript : MonoBehaviour
     }
     void spawnFruit()
     {
-        float leftmostPoint = transform.position.x - widthOffset;
-        float rightmostPoint = transform.position.x + widthOffset;
-        int index = Random.Range(0, fruits.Length);
-        GameObject chosenFruit = fruits[index];
-        Instantiate(chosenFruit, new Vector3(Random.Range(leftmostPoint, rightmostPoint), transform.position.y, 0), transform.rotation);
+        if (fruits.Length == 0) return;
+
+        float totalWeight = 0f;
+        foreach (var f in fruits)
+        {
+            totalWeight += f.weight;
+        }
+        float randomValue = Random.Range(0, totalWeight);
+        float cumulativeWeight = 0f;
+
+        foreach (var f in fruits)
+        {
+            cumulativeWeight += f.weight;
+            if (randomValue <= cumulativeWeight)
+            {
+                float leftmostPoint = transform.position.x - widthOffset;
+                float rightmostPoint = transform.position.x + widthOffset;
+                Vector3 spawnPosition = new Vector3(Random.Range(leftmostPoint, rightmostPoint), transform.position.y, 0f);
+                Instantiate(f.fruitPrefab, spawnPosition, Quaternion.identity);
+                return;
+            }
+        }
     }
 }
