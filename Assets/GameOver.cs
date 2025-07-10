35ea5c10fba1f3650b8be55060427ce202857a60
DroneDealer <1rastogiani@gmail.com>
2025-07-09 22:36:29 -0400

    I am not doing transitions anymore. coding these is sucking my life force. I give up. I will now focus on the actual game mechanics, such as what is actually sold in the shop and the tree itself

--- Assets/GameOver.cs
+++ Assets/GameOver.cs
@@ -1,24 +1,18 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using TMPro;
-using System.Collections;
 
 public class GameOver : MonoBehaviour
 {
     public GameObject gameOverCanvas;
     public TextMeshProUGUI finalScoreText;
-
     public TextMeshProUGUI highScoreText;
     public LogicScript logicScript;
-    public BasicMovements basicMovements;
     public GameObject shopWindowCanvas;
-    public FadeIn screenFade;
-
     void Start()
     {
         gameOverCanvas.SetActive(false);
-        shopWindowCanvas.SetActive(false);
     }
+
     public void GameOverNow()
     {
         logicScript.CheckHighScore();
@@ -30,19 +24,13 @@ public class GameOver : MonoBehaviour
 
     public void RestartGame()
     {
-        Debug.Log("Restart Button clicked");
         Time.timeScale = 1f;
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
     }
+
     public void GoToShop()
     {
-        StartCoroutine(CutToShop());
-    }
-    public IEnumerator CutToShop()
-    {
-        yield return screenFade.fadeIn(0.5f);
         gameOverCanvas.SetActive(false);
         shopWindowCanvas.SetActive(true);
-        yield return screenFade.FadeOut(0.5f);
     }
 }
--- Assets/ShopWindow.cs
+++ Assets/ShopWindow.cs
@@ -1,28 +1,20 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using TMPro;
 using System.Collections;
+using TMPro;
+
 public class ShopWindow : MonoBehaviour
 {
     public GameObject shopWindowCanvas;
     public GameObject gameOverCanvas;
-    public GameObject player;
-    public FadeIn screenFade;
 
     void Start()
     {
         shopWindowCanvas.SetActive(false);
     }
-    public void GoBackToGameOver()
+
+    public void ExitShop()
     {
-        StartCoroutine(ExitShop());
-    }
-    public IEnumerator ExitShop()
-    {
-        yield return screenFade.fadeIn(0.5f);
         shopWindowCanvas.SetActive(false);
         gameOverCanvas.SetActive(true);
-        yield return new WaitForSecondsRealtime(1f);
-        yield return screenFade.FadeOut(0.5f);
     }
 }
